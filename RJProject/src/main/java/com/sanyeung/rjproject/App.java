e826340a6372c0bbaa156ae64997b96201fc550b
san1991 <syq3b@mst.edu>
2014-10-31 16:56:06 -0500

    Initial work for implementing reducer jion

---
+++ RJProject/src/main/java/com/sanyeung/rjproject/App.java
@@ -0,0 +1,13 @@
+package com.sanyeung.rjproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
